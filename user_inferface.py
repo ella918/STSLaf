d30fec3ae5f19a991c2eeb52ca7b00934ac896d6
ella918 <mathewea@lafayette.edu>
2025-03-06 14:28:33 -0500

    Update user_inferface.py

--- user_inferface.py
+++ user_inferface.py
@@ -1,40 +1,162 @@
-import kivy 
-
-from kivy.uix.screenmanager import ScreenManager, Screen 
 from kivy.app import App
-from kivy.uix.image import Widget
-from kivy.uix.button import Button 
-from kivy.uix.stacklayout import StackLayout
-from kivy.config import Config
-from kivy.graphics import *
-from kivy.uix.label import Label
-from kivy.core.window import Window 
+from kivy.lang import Builder 
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import (ScreenManager, Screen, NoTransition) 
 
-Window.size = (1120,630)
+from MultObjTracking.py import FLIRFunc
 
-Config.set('graphics', 'resizable', True)
+Builder.load_string("""
 
-class HomeScreen(App):
-	def build(self):
-		
-		SL = StackLayout(orientation='tb-lr')
-		
-		btn_setup = Button(text='Set Up/Begin Sensing', font_size = 15, size_hint = (1, .25))
-		btn_connection = Button(text='Check Sensor Connection', font_size = 15, size_hint = (1, .25))
-		btn_crash = Button(text='Override: Crash', font_size = 15, size_hint = (1, .25))
-		btn_no_crash = Button(text='Override: No Crash', font_size = 15, size_hint = (1, .25))
-		
-		btn_setup.bind(on_press = self.callback)
-		
-		SL.add_widget(btn_setup)
-		SL.add_widget(btn_connection)
-		SL.add_widget(btn_crash)
-		SL.add_widget(btn_no_crash)
-		
-		return SL
-		
-		def callback(self,event):
-			print("Set Up/Begin Sensing Pressed")
-		
+<HomeScreen>:
+	id: main_win 
+    orientation: "vertical"
+    spacing: 10
+    space_x: self.size[0]/3
+  
+    canvas.before: 
+        Color: 
+            rgba: (125, 14, 20, 0.74) 
+        Rectangle: 
+            source:'sts.png'
+            size: root.width, root.height 
+            pos: self.pos 
+
+    BoxLayout:
+        Button:
+            text: 'Set Up/Begin Sensing'
+            font_size: 30
+            size_hint: (1, .2)
+            background_color: (0.1, .36, .4, .75)
+            on_press: root.manager.current = 'setup'
+        Button:
+            text: 'Check Sensor Connection'
+            font_size: 30
+            size_hint: (1, .2)
+            background_color: (0.1, .36, .4, .75)
+            on_press: root.manager.current = 'connection'
+        Button:
+            text: 'Override: Crash'
+            font_size: 30
+            size_hint: (1, .2)
+            background_color: (0.1, .36, .4, .75)
+            on_press: root.manager.current = 'crash'
+        Button:
+            text: 'Override: No Crash'
+            font_size: 30
+            size_hint: (1, .2)
+            background_color: (0.1, .36, .4, .75)
+            on_press: root.manager.current = 'nocrash'
+        Button:
+            text: 'Stop Sensing'
+            font_size: 30
+            size_hint: (1, .2)
+            background_color: (0.1, .36, .4, .75)
+            on_press: root.manager.current = 'stop'
+
+<SetUpScreen>:
+    BoxLayout:
+        Button:
+            text: 'my set up button'
+            ont_size: 30
+            size_hint: (1, .5)
+            background_color: (0.1, .36, .4, .75)
+        Button:
+            text: 'Back to home screen'
+            ont_size: 30
+            size_hint: (1, .5)
+            background_color: (0.1, .36, .4, .75)
+            on_press: root.manager.current = 'home'
+<ConnectionScreen>:
+    BoxLayout:
+        Button:
+            text: 'My sensor connection button'
+            ont_size: 30
+            size_hint: (1, .5)
+            background_color: (0.1, .36, .4, .75)
+        Button:
+            text: 'Back to home screen'
+            ont_size: 30
+            size_hint: (1, .5)
+            background_color: (0.1, .36, .4, .75)
+            on_press: root.manager.current = 'home'
+<CrashScreen>:
+    BoxLayout:
+        Button:
+            text: 'My override crash button'
+            ont_size: 30
+            size_hint: (1, .5)
+            background_color: (0.1, .36, .4, .75)
+        Button:
+            text: 'Back to home screen'
+            ont_size: 30
+            size_hint: (1, .5)
+            background_color: (0.1, .36, .4, .75)
+            on_press: root.manager.current = 'home'
+<NoCrashScreen>:
+    BoxLayout:
+        Button:
+            text: 'My override: no crash button'
+            ont_size: 30
+            size_hint: (1, .5)
+            background_color: (0.1, .36, .4, .75)
+        Button:
+            text: 'Back to home screen'
+            ont_size: 30
+            size_hint: (1, .5)
+            background_color: (0.1, .36, .4, .75)
+            on_press: root.manager.current = 'home'
+<StopScreen>:
+    BoxLayout:
+        Button:
+            text: 'My stop sensing button'
+            ont_size: 30
+            size_hint: (1, .5)
+            background_color: (0.1, .36, .4, .75)
+        Button:
+            text: 'Back to home screen'
+            ont_size: 30
+            size_hint: (1, .5)
+            background_color: (0.1, .36, .4, .75)
+            on_press: root.manager.current = 'home'
+""")
+
+# Declare both screens
+
+class HomeScreen(Screen):
+    pass
+
+class SetUpScreen(Screen):
+    def on_enter(self):
+    	print('woooo')
+    	FlirFunc()
+
+class ConnectionScreen(Screen):
+    pass
+
+class CrashScreen(Screen):
+    pass
+
+class NoCrashScreen(Screen):
+    pass
+
+class StopScreen(Screen):
+    pass
+
+
+class FLIRApp(App):
+
+    def build(self):
+        # Create the screen manager
+        sm = ScreenManager(transition = NoTransition())
+        sm.add_widget(HomeScreen(name='home'))
+        sm.add_widget(SetUpScreen(name='setup'))
+        sm.add_widget(ConnectionScreen(name='connection'))
+        sm.add_widget(CrashScreen(name='crash'))
+        sm.add_widget(NoCrashScreen(name='nocrash'))
+        sm.add_widget(StopScreen(name='stop'))
+
+        return sm
+
+
 if __name__ == '__main__':
-	HomeScreen().run()
+    FLIRApp().run()
